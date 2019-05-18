482297e54a8381fa1f831e13306a2104446cdb77
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-05-18 09:43:12 -0700

    connect with mongoose

---
+++ db/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${connectionString}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log(`Mongoose is disconnected`);
+});
+
+mongoose.connection.on("error", err => {
+  console.log(err, "mongoose error");
+});
